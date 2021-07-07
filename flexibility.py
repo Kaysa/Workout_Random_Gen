e3fce965c4dcebb0dc88bd9065611add01a77fb3
Kaylah <KaylahMack24@gmail.com>
2021-07-07 17:26:59 -0400

    added Flexibility items

---
+++ flexibility.py
@@ -0,0 +1,24 @@
+flexy = ("Forward Lunges",
+         "Side Lunges",
+         "Cross-Over",
+         "Standing Quad Stretch",
+         "Seat Straddle Lotus",
+         "Seat Side Straddle",
+         "Seat Stretch",
+         "Knees to Chest",
+         "Hamstring Stretches",
+         "Instep Hamstring Stretch",
+         "Hip Flexor Stretches",
+         "Pigeon Stretch",
+         "Calf Stretch",
+         "Back Stretches",
+         " Child’s Pose",
+         "Lying Knee Twist",
+         "Core Stretches",
+         " Standing Oblique Stretch",
+         "Glute Stretches",
+         "Twisting Glute Stretch"
+
+
+
+)
